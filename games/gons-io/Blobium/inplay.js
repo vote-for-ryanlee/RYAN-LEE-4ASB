1426233d2eec78b87bac329ec51c4105633e225e
@RyBoi388 <148025143+TheHackerBoi123@users.noreply.github.com>
2024-09-05 19:52:14 -0700

    No ads on gons.io!

--- games/gons-io/Blobium/inplay.js
+++ games/gons-io/Blobium/inplay.js
@@ -1,33 +1,3 @@
-function initAipPreroll() {
-	if(typeof aipPlayer != "undefined") {
-		adplayer = new aipPlayer({
-			AD_WIDTH: 960,
-			AD_HEIGHT: 540,
-			AD_FULLSCREEN: true,
-			AD_CENTERPLAYER: false,
-			LOADING_TEXT: 'loading advertisement',
-			PREROLL_ELEM: document.getElementById('preroll'),
-			AIP_COMPLETE: function ()  {
-				/*******************
-				 ***** WARNING *****
-				 *******************
-				 Please do not remove the PREROLL_ELEM
-				 from the page, it will be hidden automaticly.
-				 If you do want to remove it use the AIP_REMOVE callback below
-				*/
-				//alert("Ad Completed");
-			},
-			AIP_REMOVE: function ()  {
-				// Here it's save to remove the PREROLL_ELEM from the page
-				// But it's not necessary
-			}
-		});
-	} else {
-		// Failed to load the adslib ads are probably blocked
-		// don't call the startPreRoll function.
-		// it will result in an error.
-	}
-}
 
 function getScript (src, callback) {
 	var headElm = document.head || document.getElementsByTagName('head')[0];
